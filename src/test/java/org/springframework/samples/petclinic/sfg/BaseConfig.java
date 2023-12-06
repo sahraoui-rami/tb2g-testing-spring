fc73ddabd9ac34249a792ec58801158db9c81acd
ChefServiceInfo <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>
2023-12-06 17:52:01 +0100

    JUnit 4 Laurel Test.

---
+++ src/test/java/org/springframework/samples/petclinic/sfg/BaseConfig.java
@@ -0,0 +1,15 @@
+package org.springframework.samples.petclinic.sfg;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Rami SAHRAOUI on 06/12/2023
+ */
+@Configuration
+class BaseConfig {
+    @Bean
+    HearingInterpreter hearingInterpreter(WordProducer wordProducer) {
+        return new HearingInterpreter(wordProducer);
+    }
+}
---
+++ src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterTest.java
@@ -0,0 +1,34 @@
+package org.springframework.samples.petclinic.sfg;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Rami SAHRAOUI on 06/12/2023
+ */
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {BaseConfig.class, LaurelConfig.class})
+public class HearingInterpreterTest {
+
+    @Autowired
+    HearingInterpreter hearingInterpreter;
+
+// without the Spring Context
+//    @Before
+//    public void setUp() throws Exception {
+//        hearingInterpreter = new HearingInterpreter(new LaurelWordProducer());
+//    }
+
+    @Test
+    public void whatIHeard() {
+        String word = hearingInterpreter.whatIHeard();
+
+        assertEquals("Laurel", word);
+    }
+}
