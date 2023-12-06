d7c278be4ae63103f1febdc3cdb40082357ece77
ChefServiceInfo <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>
2023-12-06 18:01:00 +0100

    Assignment - JUnit 4 Yanny Test.

---
+++ src/test/java/org/springframework/samples/petclinic/sfg/YannyConfig.java
@@ -0,0 +1,15 @@
+package org.springframework.samples.petclinic.sfg;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Rami SAHRAOUI on 06/12/2023
+ */
+@Configuration
+public class YannyConfig {
+    @Bean
+    YannyWordProducer yannyWordProducer() {
+        return new YannyWordProducer();
+    }
+}
